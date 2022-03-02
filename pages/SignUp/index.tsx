142fc73c1f796dbbd10ae051bb4d38cb6bd3aef8
seo722 <95078290+seo722@users.noreply.github.com>
2022-03-02 13:27:15 +0900

    login,signup 끝

--- pages/SignUp/index.tsx
+++ pages/SignUp/index.tsx
@@ -1,18 +1,76 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { Button, Form, Header, Input, Label, LinkContainer } from "./styles";
+import useInput from "@hooks/useInput";
+import React, { useCallback, useState } from "react";
+import { Link, Navigate, Route, Routes } from "react-router-dom";
+import { Button, Error, Form, Header, Input, Label, LinkContainer, Success } from "./styles";
+import axios from "axios";
+import useSWR from "swr";
+import fetcher from "@utils/fetcher";
 
 const SignUp = () => {
-  const [email] = useState("");
-  const [nickname] = useState("");
-  const [password] = useState("");
-  const [passwordCheck] = useState("");
+  const { data, error, mutate } = useSWR("http://localhost:3095/api/users", fetcher, {
+    // dedupingInterval: 10000,
+  });
+  const [email, onChangeEmail] = useInput("");
+  const [nickname, onChangeNickname] = useInput("");
+  const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [misMatchError, setMisMatchError] = useState(false);
+  const [signUpError, setSignUpError] = useState("");
+  const [signUpSuccess, setSignUpSuccess] = useState(false);
 
-  const onSubmit = () => {};
-  const onChangeEmail = () => {};
-  const onChangeNickname = () => {};
-  const onChangePassword = () => {};
-  const onChangePasswordCheck = () => {};
+  const onChangePassword = useCallback(
+    (e) => {
+      setPassword(e.target.value);
+      setMisMatchError(e.target.value !== passwordCheck);
+    },
+    [passwordCheck],
+  );
+
+  const onChangePasswordCheck = useCallback(
+    (e) => {
+      setPasswordCheck(e.target.value);
+      setMisMatchError(e.target.value !== password);
+    },
+    [password],
+  );
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      console.log(email, nickname, password, passwordCheck);
+      if (!misMatchError) {
+        console.log("서버로 회원가입");
+        setSignUpError("");
+        setSignUpSuccess(false);
+        axios
+          .post("http://localhost:3095/api/users", { email, nickname, password })
+          .then((response) => {
+            console.log(response);
+            setSignUpSuccess(true);
+          })
+          .catch((error) => {
+            console.log(error.response);
+            setSignUpError(error.response.data);
+          })
+          .finally(() => {});
+      }
+    },
+    [email, nickname, password, passwordCheck, misMatchError],
+  );
+
+  if (data === undefined) {
+    return <div>로딩중...</div>;
+  }
+
+  if (!error && data) {
+    console.log("로그인", data);
+    //return 은 항상 hooks 밑에 있어야됨
+    return (
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/workspace/channel" />} />
+      </Routes>
+    );
+  }
 
   return (
     <div id="container">
@@ -27,13 +85,25 @@ const SignUp = () => {
         <Label id="nickname-label">
           <span>닉네임</span>
           <div>
-            <Input type="text" id="nickname" name="nickname" value={nickname} onChange={onChangeNickname} />
+            <Input
+              type="text"
+              id="nickname"
+              name="nickname"
+              value={nickname}
+              onChange={onChangeNickname}
+            />
           </div>
         </Label>
         <Label id="password-label">
           <span>비밀번호</span>
           <div>
-            <Input type="password" id="password" name="password" value={password} onChange={onChangePassword} />
+            <Input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={onChangePassword}
+            />
           </div>
         </Label>
         <Label id="password-check-label">
@@ -47,10 +117,10 @@ const SignUp = () => {
               onChange={onChangePasswordCheck}
             />
           </div>
-          {/* {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
+          {misMatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
           {!nickname && <Error>닉네임을 입력해주세요.</Error>}
           {signUpError && <Error>{signUpError}</Error>}
-          {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>} */}
+          {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>}
         </Label>
         <Button type="submit">회원가입</Button>
       </Form>
---
+++ utils/fetcher.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const fetcher = <Data>(url: string) =>
+  axios
+    .get<Data>(url, {
+      withCredentials: true,
+    })
+    .then((response) => response.data);
+
+export default fetcher;
